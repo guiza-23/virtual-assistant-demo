8087380033d428382ccc8b70a2c573d013009637
cedric cazal <cedcaz@gmail.com>
2022-08-30 14:23:03 -0500

    sign up desactived validation type_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
   has_many :reviews
 
-  validates :type_of_user, inclusion: { in: TYPES }
-  validates :username, uniqueness: { scope: :username, message: "username already taken" }
+  # validates :type_of_user, inclusion: { in: TYPES }
+  # validates :username, uniqueness: { scope: :username, message: "username already taken" }
   validates :email, uniqueness: { scope: :email, message: "email already has an account" }
   # validates :avatar, presence: true
   # has_one_attached :avatar
