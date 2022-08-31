c7fa37072a2bbb2e6e462f580c75aaa3e040de02
cedric cazal <cedcaz@gmail.com>
2022-08-31 11:49:09 -0500

    controller offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,6 @@
 class OffersController < ApplicationController
+  before_action :set_user
+  before_action :set_offer, only: %i[show edit update destroy]
 
   def index
     @offers = Offer.all
@@ -9,25 +11,51 @@ class OffersController < ApplicationController
   end
 
   def new
-    @offer = Offer.new
+    if @user.company != nil
+      @offer = Offer.new
+    else
+      redirect_to new_company_path
     # authorize @offer
+    end
   end
 
   def create
     @offer = Offer.new(offer_params)
-    @offer.company_id = 1
+    @offer.company_id = @user.company.id
     if @offer.save
       redirect_to offers_path
     else
-      raise
       render :new, status: :unprocessable_entity
     end
     # authorize @offer
   end
 
+  def edit; end
+
+  def update
+    if @offer.update(offer_params)
+      redirect_to offer_path
+    else
+      render "Edit"
+    end
+  end
+
+  def destroy
+    @offer.destroy
+    redirect_to offers_path
+  end
+
   private
 
   def offer_params
-    params.require(:offer).permit(:description, :tag)
+    params.require(:offer).permit(:description, :tag, :company_id)
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
   end
 end
