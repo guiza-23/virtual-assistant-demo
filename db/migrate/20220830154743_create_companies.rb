67c49b2d7b0ce29c41cd9bb9cbb8ba9df1d8226e
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-08-31 13:50:44 -0300

    company controller

--- db/migrate/20220830154743_create_companies.rb
+++ db/migrate/20220830154743_create_companies.rb
@@ -1,7 +1,8 @@
 class CreateCompanies < ActiveRecord::Migration[7.0]
   def change
     create_table :companies do |t|
-      t.text :description, presence: true
+      t.string :name
+      t.text :description
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
