120b469bcba95cb7a049297971e3192fd1a96b5c
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-09-02 15:05:34 -0300

    added photo validations to company and database files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,21 @@
 #   Character.create(name: "Luke", movie: movies.first)
 User.destroy_all
 
-user1 = User.new(username: 'cocacola', email: 'coca-cola@gmail.com', password: '123456', type_of_user: 'Company', address: 'niceto vega 1234', phone: '15-15488956')
+user1 = User.new(username: 'Cocacola', email: 'coca-cola@gmail.com', password: '123456', type_of_user: 'Company', address: 'Niceto Vega 1234, Minessotta, USA', phone: '15-15488956')
 user1.save!
-user2 = User.new(username: 'pedro', email: 'pedropiedra@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'vegarr 1234', phone: '15-4545454')
+user2 = User.new(username: 'Pedro', email: 'pedropiedra@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Independence Av. 3004, London, UK', phone: '15-4545454')
 user2.save!
-user3 = User.new(username: 'coco', email: 'cocochanel@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'bogota 1234', phone: '15-6666666')
+user3 = User.new(username: 'Coco', email: 'cocochanel@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Bogota 1234, Lima, Peru', phone: '15-6666666')
 user3.save!
-user4 = User.new(username: 'Netflix', email: 'netflix@gmail.com', password: '123456', type_of_user: 'Company', address: 'Buenos Aires 1234', phone: '15-6666645')
+user4 = User.new(username: 'Netflix', email: 'netflix@gmail.com', password: '123456', type_of_user: 'Company', address: 'Buenos Aires 504, Buenos Aires, ARG', phone: '15-6666645')
 user4.save!
 
 
-company1 = Company.new(user_id: user1.id, name: 'coca-cola', description: 'la empresa mas grande del mundo en venta de bebidas gaseosas saborizadas')
-company2 = Company.new(user_id: user4.id, name: 'Netflix', description: 'la empresa mas grande de peliculas del mundo')
+company1 = Company.new(user_id: user1.id, name: 'Coca-cola', description: 'The largest company in the world in the sale of flavored soft drinks')
+company1.photo.attach(io: URI.open("https://images.unsplash.com/photo-1622483767028-3f66f32aef97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"), filename: "company1.png", content_type: "image/jpeg")
 company1.save!
+company2 = Company.new(user_id: user4.id, name: 'Netflix', description: 'The biggest streaming movie company in the world')
+company2.photo.attach(io: URI.open("https://images.unsplash.com/photo-1661077150377-26922fb352bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"), filename: "company2.png", content_type: "image/jpeg")
 company2.save!
 
 assistant1 = Assistant.new(user_id: user2.id, skills: "Computer Software", availability: 'Full-time', firstname: "Pedro", lastname: "Piedra")
@@ -29,9 +31,9 @@ assistant2 = Assistant.new(user_id: user3.id, skills: "Flexibility", availabilit
 assistant2.photo.attach(io: URI.open("https://images.unsplash.com/photo-1629747387925-6905ff5a558a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"), filename: "assistant2.png", content_type: "image/jpeg")
 assistant2.save!
 
-offer1 = Offer.new(company_id: company1.id, title: 'Asistente para oficina en Minessota', description: 'busco asistente para 5 personas', tag: 'part-time')
-offer2 = Offer.new(company_id: company2.id, title: 'Employees for LA office', description: 'busco asistente para 2 personas', tag: 'full-time')
-offer3 = Offer.new(company_id: company1.id, title: 'Looking for remote assistant', description: ' asistente para 3 personas', tag: 'full-time')
+offer1 = Offer.new(company_id: company1.id, title: 'Administrative assistant for an office in Minnesota', description: 'I am looking for a virtual assistant for 5 people', tag: 'part-time')
+offer2 = Offer.new(company_id: company2.id, title: 'An organizational tasks virtual assistant for employees for LA office', description: 'I am looking for an assistant for 2 people', tag: 'full-time')
+offer3 = Offer.new(company_id: company1.id, title: 'Looking for remote bilingual assistant', description: ' Assistant for 3 people', tag: 'full-time')
 
 offer1.save!
 offer2.save!
