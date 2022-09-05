e0dc024bbb2a7f2e23515939fd15218fd76ea161
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-09-05 14:44:49 -0300

    added comment validations and routes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
+  validates :title, length: { minimum: 5, maximum: 20 }
   validates :content, presence: true
   validates :content, length: { minimum: 20, maximum: 100 }
 end
