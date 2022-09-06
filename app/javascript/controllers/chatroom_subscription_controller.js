913a3ec671af9bb82589969e33be5b535bfedfd5
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-09-06 13:30:10 -0300

    chat funcionando y con estilo

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
   static targets = ["mensajes", "inputs"]
 
   connect() {
@@ -14,11 +14,31 @@ export default class extends Controller {
   }
 
   #insertMessageAndScrollDown(data) {
-    this.mensajesTarget.insertAdjacentHTML("beforeend", data);
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.mensaje)
+
+    this.mensajesTarget.insertAdjacentHTML("beforeend", messageElement);
     this.mensajesTarget.scrollTo(0, this.mensajesTarget.scrollHeight);
     console.log(this.inputsTarget)
 
+  }
 
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 
   resetForm(event) {
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,8 @@ class Company < ApplicationRecord
   belongs_to :user
   has_many :offers, dependent: :destroy
   has_many :applications, through: :offers
+  has_many :chatrooms, dependent: :destroy
+  has_many :mensajes, through: :offers
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
