3727de8dc350d93a3e156b251373c176f911a1fb
cedric cazal <cedcaz@gmail.com>
2022-09-07 08:59:17 -0500

    add type_js implementado

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
 
-// Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
+    new Typed(this.element, {
+      strings: ["Change your life", "Find your dream in 1 click !"],
+      typeSpeed: 100,
+      loop: true
+    })
   }
 }
