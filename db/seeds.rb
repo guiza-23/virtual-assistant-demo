018e72811f6a546d8ef415b76cfdfd889170ece2
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-09-07 15:05:58 -0300

    added 6 companies and view index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@ User.destroy_all
 puts "Data Base CLEANED"
 
 
-user1 = User.new(username: 'Coca cola', email: 'coca-cola@gmail.com', password: '123456', type_of_user: 'Company', address: 'Niceto Vega 1234, Minessotta, United States', phone: '+16512014899')
+user1 = User.new(username: 'Coca-Cola', email: 'coca-cola@gmail.com', password: '123456', type_of_user: 'Company', address: 'Niceto Vega 1234, Minessotta, United States', phone: '+1 (404) 676-2121')
 user1.save!
 user2 = User.new(username: 'Pedro', email: 'pedro@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Independence Av. 3004, London, United Kingdom', phone: '+442065789809')
 user2.save!
 user3 = User.new(username: 'Emma', email: 'emma@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Bogota 1234, Lima, Peru', phone: '+510191236767')
 user3.save!
-user4 = User.new(username: 'Netflix', email: 'netflix@gmail.com', password: '123456', type_of_user: 'Company', address: 'Buenos Aires 504, Buenos Aires, Argentina', phone: '+54911678986')
+user4 = User.new(username: 'Netflix', email: 'netflix@gmail.com', password: '123456', type_of_user: 'Company', address: '100 Winchester Cir, California, United States', phone: '+1 408-540-3700')
 user4.save!
 user5 = User.new(username: 'Sarah', email: 'sarah@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Saint James 3005, Paris, France', phone: '+33140250808')
 user5.save!
@@ -21,15 +21,52 @@ user7 = User.new(username: 'Abril', email: 'abril@gmail.com', password: '123456'
 user7.save!
 user8 = User.new(username: 'Jose', email: 'jose@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Street: Cr 54 No., Medellin, Colombia', phone: '+57617896283')
 user8.save!
+user9 = User.new(username: 'Austin', email: 'austin@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Framveien 114, Oslo, Norway', phone: '+4741544777')
+user9.save!
+user10 = User.new(username: 'Simon', email: 'simon@gmail.com', password: '123456', type_of_user: 'Assistant', address: '21 Baker Street, Millbrook, Australia', phone: '+6141544087')
+user10.save!
+user11 = User.new(username: 'Alex', email: 'alex@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Lebedeva, bld. 7, Cheboksary, Russia', phone: '+7-41544087')
+user11.save!
+user12 = User.new(username: 'Yannaty', email: 'yannaty@gmail.com', password: '123456', type_of_user: 'Assistant', address: '1849 St. John Street, Caledon, South Africa', phone: '+2741884087')
+user12.save!
+user13 = User.new(username: 'Laura', email: 'laura@gmail.com', password: '123456', type_of_user: 'Assistant', address: 'Av. Coronel Bóveda, Quiíndy, Paraguay', phone: '+595276353')
+user13.save!
+user14 = User.new(username: 'Jimena', email: 'jimena@gmail.com', password: '123456', type_of_user: 'Assistant', address: '2311 5th Avenue, Toronto, Canada', phone: '+1-95276353')
+user14.save!
 
+user15 = User.new(username: 'Instagram', email: 'instagram@gmail.com', password: '123456', type_of_user: 'Company', address: '181 Fremont St, San Francisco, United States', phone: '+1-650-543-4800')
+user15.save!
+user16 = User.new(username: 'McDonalds', email: 'mcdonalds@gmail.com', password: '123456', type_of_user: 'Company', address: '181 Fremont St, San Francisco, United States', phone: '+1-650-543-4800')
+user16.save!
+user17 = User.new(username: 'Facebook', email: 'facebook@gmail.com', password: '123456', type_of_user: 'Company', address: '1601 Willow Rd., Menlo Park, United States', phone: '+1 (650) 308-7300')
+user17.save!
+user18 = User.new(username: 'Google LLC', email: 'google@gmail.com', password: '123456', type_of_user: 'Company', address: '1600 Amphitheatre Parkway Mountain View, CA, United States', phone: '+1 (650) 309-7355')
+user18.save!
 puts "Created USERS"
 
-company1 = Company.new(user_id: user1.id, name: 'Coca-cola', description: 'The largest company in the world in the sale of flavored soft drinks')
+company1 = Company.new(user_id: user1.id, name: 'Coca-Cola Co.', description: 'The Coca-Cola Co. is the nonalcoholic beverage company, which engages in the manufacture, market, and sale of non-alcoholic beverages which include sparkling soft drinks, water, enhanced water and sports drinks, juice, dairy and plant-based beverages, tea and coffee and energy drinks.')
 company1.photo.attach(io: URI.open("https://images.unsplash.com/photo-1622483767028-3f66f32aef97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"), filename: "company1.png", content_type: "image/jpeg")
 company1.save!
-company2 = Company.new(user_id: user4.id, name: 'Netflix', description: 'The biggest streaming movie company in the world')
+
+company2 = Company.new(user_id: user4.id, name: 'Netflix Inc', description: 'Netflix Inc (Netflix) provides internet entertainment services for watching movies and television shows. The company offers TV shows and movies such as original series, documentaries, and feature films through an internet subscription on the TV, computer, and mobile devices.')
 company2.photo.attach(io: URI.open("https://images.unsplash.com/photo-1661077150377-26922fb352bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"), filename: "company2.png", content_type: "image/jpeg")
 company2.save!
+
+company3 = Company.new(user_id: user15.id, name: 'Instagram', description: 'Instagram is a free, online photo-sharing application and social network platform that was acquired by Facebook in 2012. Instagram allows users to edit and upload photos and short videos through a mobile app.')
+company3.photo.attach(io: URI.open("https://images.unsplash.com/photo-1633886897663-44c707d71904?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "company3.png", content_type: "image/jpeg")
+company3.save!
+
+company4 = Company.new(user_id: user16.id, name: 'McDonalds Co.', description: 'McDonalds is the worlds largest restaurant chain by revenue, serving over 69 million customers daily in over 100 countries in more than 40.000 outlets as of 2021. McDonalds is best known for its hamburgers, cheeseburgers and french fries, although their menus include other items like chicken, fish, fruit, and salads.')
+company4.photo.attach(io: URI.open("https://images.unsplash.com/photo-1619454016518-697bc231e7cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "company4.png", content_type: "image/jpeg")
+company4.save!
+
+company5 = Company.new(user_id: user17.id, name: 'Facebook', description: 'Facebook is a website which allows users, who sign-up for free profiles, to connect with friends, work colleagues or people they dont know, online. It allows users to share pictures, music, videos, and articles, as well as their own thoughts and opinions with however many people they like.')
+company5.photo.attach(io: URI.open("https://images.unsplash.com/photo-1627843563095-f6e94676cfe0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "company5.png", content_type: "image/jpeg")
+company5.save!
+
+company6 = Company.new(user_id: user18.id, name: 'Google LLC', description: 'Google LLC (Google), a subsidiary of Alphabet Inc, is a provider of search and advertising services on the internet. The company focuses on business areas such as advertising, search, platforms and operating systems, and enterprise and hardware products.')
+company6.photo.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662568753/development/New_Logo_for_Google_BP_O_mwf8xy.jpg"), filename: "company6.png", content_type: "image/jpeg")
+company6.save!
 
 puts "Created COMPANIES"
 
@@ -63,7 +100,37 @@ assistant6.photo.attach(io: URI.open("https://images.unsplash.com/photo-16236059
 assistant6.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant6.pdf", content_type: "file/pdf")
 assistant6.save!
 
-puts "Created OFFERS"
+assistant7 = Assistant.new(user_id: user9.id, skills: "Flexibility", availability: 'Part-time', firstname: "Austin", lastname: "Wade")
+assistant7.photo.attach(io: URI.open("https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "assistant7.png", content_type: "image/jpeg")
+assistant7.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant7.pdf", content_type: "file/pdf")
+assistant7.save!
+
+assistant8 = Assistant.new(user_id: user10.id, skills: "Flexibility", availability: 'Full-time', firstname: "Simon", lastname: "Lohmann")
+assistant8.photo.attach(io: URI.open("https://images.unsplash.com/photo-1613064756072-52b429a1e06f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "assistant8.png", content_type: "image/jpeg")
+assistant8.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant8.pdf", content_type: "file/pdf")
+assistant8.save!
+
+assistant9 = Assistant.new(user_id: user11.id, skills: "Flexibility", availability: 'Full-time', firstname: "Alex", lastname: "Supranoff")
+assistant9.photo.attach(io: URI.open("https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "assistant9.png", content_type: "image/jpeg")
+assistant9.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant9.pdf", content_type: "file/pdf")
+assistant9.save!
+
+assistant10 = Assistant.new(user_id: user12.id, skills: "Flexibility", availability: 'Full-time', firstname: "Yannaty", lastname: "Kouth")
+assistant10.photo.attach(io: URI.open("https://images.unsplash.com/photo-1654086438689-ac593fb9387a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=527&q=80"), filename: "assistant10.png", content_type: "image/jpeg")
+assistant10.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant10.pdf", content_type: "file/pdf")
+assistant10.save!
+
+assistant11 = Assistant.new(user_id: user13.id, skills: "Flexibility", availability: 'Full-time', firstname: "Laura", lastname: "Alonso")
+assistant11.photo.attach(io: URI.open("https://images.unsplash.com/photo-1615751596346-9df8006e5381?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"), filename: "assistant11.png", content_type: "image/jpeg")
+assistant11.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant11.pdf", content_type: "file/pdf")
+assistant11.save!
+
+assistant12 = Assistant.new(user_id: user14.id, skills: "Flexibility", availability: 'Full-time', firstname: "Jimena", lastname: "Rodriguez")
+assistant12.photo.attach(io: URI.open("https://images.unsplash.com/photo-1639619628924-eced0acbab4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=583&q=80"), filename: "assistant12.png", content_type: "image/jpeg")
+assistant12.cv.attach(io: URI.open("https://res.cloudinary.com/watchlistcreator/image/upload/v1662561533/development/Documento_sin_t%C3%ADtulo_1_atwxrz.pdf"), filename: "assistant12.pdf", content_type: "file/pdf")
+assistant12.save!
+
+puts "Created ASSISTANTS"
 
 offer1 = Offer.new(company_id: company1.id, title: 'Administrative assistant', location: 'Minnesota', description: 'Looking for a virtual assistant for 5 employees', cost_hour: 25, tag: 'Part-time')
 offer1.save!
@@ -72,7 +139,7 @@ offer2.save!
 offer3 = Offer.new(company_id: company1.id, title: 'A remote bilingual', location: 'Buenos Aires', description: 'Bilingual assistant for 3 people', cost_hour: 18, tag: 'Any')
 offer3.save!
 
-puts "Created APPLICATIONS"
+puts "Created OFFERS"
 
 application1 = Application.new(assistant_id: assistant1.id, offer_id: offer1.id, status: "Interview")
 application1.save!
@@ -85,14 +152,15 @@ application4.save!
 application5 = Application.new(assistant_id: assistant1.id, offer_id: offer2.id, status: "Interview")
 application5.save!
 
-puts "Created REVIEWS"
+puts "Created APPLICATIONS"
+
 
 review1 = Review.new(assistant_id: assistant1.id, content: 'Excellent assistant!', rating: 5)
 review1.save!
 review2 = Review.new(assistant_id: assistant2.id, content: 'Amazing bilingual assistant!', rating: 4)
 review2.save!
 
-puts "Created COMMENTS"
+puts "Created REVIEWS"
 
 comment1 = Comment.new(title: 'Amazing Job', content: 'It allows people with skills like me all over the world be able to work remote/online', rating: 5, name: "Pedro", assistant_id: assistant1.id)
 comment1.save!
@@ -100,5 +168,7 @@ comment2 = Comment.new(title: 'Great platform', content: 'I was able to work rem
 comment2.save!
 comment3 = Comment.new(title: 'Excellent', content: 'I am working remote with an excellent salary and getting to know great companies', rating: 5, name: "Anonymous", assistant_id: assistant1.id)
 comment3.save!
+
+puts "Created COMMENTS"
 
 puts "FINISH --- SEED CREATED"
